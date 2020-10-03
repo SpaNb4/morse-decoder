9453fd0efad2b0854c89a4a0fbd4f50af40c03e5
SpaNb4 <spanb4@gmail.com>
2020-10-03 16:19:28 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let n = 0;
+    let q = 0;
+    for (let i = 0; i < expr.length / 10; i++) {
+        n += 10;
+        arr[i] = expr.substring(q, n);
+        q += 10;
+    }
+    n = 0;
+    q = 0;
+    let str = '';
+    let result = '';
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].replace(/^0+/, '');
+        for (let j = 0; j < arr[i].length / 2; j++) {
+            n += 2;
+            if (arr[i].substring(q, n) == '11') {
+                str += '-';
+            }
+            else if (arr[i].substring(q, n) == '10') {
+                str += '.';
+            }
+            else {
+                str += ' ';
+            }
+            q += 2;
+        }
+        if (str[0] == ' ') {
+            result += ' ';
+        }
+        else {
+            result += MORSE_TABLE[str];
+        }
+        str = '';
+        q = 0;
+        n = 0;
+    }
+    return result;
 }
 
 module.exports = {
